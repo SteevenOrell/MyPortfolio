d86533ff6b257f2300e717e929ae7146ec7df755
SteevenOrell <nunkediesteevenorelljunior.wemin@georgebrown.ca>
2025-10-27 19:18:31 -0400

    Update 3.1

--- src/components/SlideInAnimation.js
+++ src/components/SlideInAnimation.js
@@ -6,7 +6,7 @@ export  const SlideInAnimation = ({ children }) => {
     <motion.div
       initial={{ opacity: 0, x: 100 }} // Initial state: invisible and slightly below its final position
       whileInView={{ opacity: 1, x: 0 }} // Animate to: fully visible and in its original X position
-      viewport={{ once: false, amount: 0.5 }} // Trigger when 50% of the item is in view, only once
+      viewport={{ once: true, amount: 0.5 }} // Trigger when 50% of the item is in view, only once
       transition={{ duration: 0.8, ease: "easeOut" }} 
     >
       {children}
